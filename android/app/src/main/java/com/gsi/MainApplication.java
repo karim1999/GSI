56da9f28e4ad52645b4b8186ce7e0ed536bc2deb
Karim Mahmoud <karim.elbadry2@gmail.com>
2018-12-30 13:36:36 +0200

    test2

--- android/app/src/main/java/com/gsi/MainApplication.java
+++ android/app/src/main/java/com/gsi/MainApplication.java
@@ -3,14 +3,10 @@ package com.gsi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
-import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,11 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNFirebasePackage(),
-            new ImagePickerPackage(),
-            new RNFirebaseMessagingPackage(),
-            new RNFirebaseNotificationsPackage()
+          new MainReactPackage()
       );
     }
 
